3bb967c0ea32fe9f45152bc97db95b7f3d838e38
yas-mos <yasse1r@gmail.com>
2024-09-03 04:46:41 +0300

    Assignments
    Assignment solutions

---
+++ Assignment/02/script.js
@@ -0,0 +1,18 @@
+let x = Math.round (Math.random() * 100);
+let y = Math.round (Math.random() * 50);
+console.log( "X = " + x + " And Y = " + y);
+
+let sum = x + y;
+console.log( "Sum = " + sum);
+
+let substract = x - y;
+console.log("Substraction = " + substract);
+
+let multi = x * y;
+console.log("Multiplying = " + multi);
+
+let div = x / y;
+console.log("Divide = " + div);
+
+let remainder = x % y;
+console.log("Remainder = " + remainder);
